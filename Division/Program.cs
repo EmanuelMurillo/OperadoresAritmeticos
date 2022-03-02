93fdd24379c32895f05f9b62f0dd210e6c51a2fc
francisco.emanuel.murillo@gmail.com <francisco.emanuel.murillo@gmail.com>
2022-03-01 19:32:23 -0800

    OperadoresAritmeticos

---
+++ Division/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1 = 10;
+            int Num2 = 2;
+
+            Console.WriteLine(Num1 / Num2);
+            Console.ReadKey();
+        }
+    }
+}
